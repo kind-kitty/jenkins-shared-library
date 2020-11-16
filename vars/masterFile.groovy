3fa878f03c0ed255d9475f6981fed920176590fd
Kavya K <srikavyakola2285@gmail.com>
2020-11-16 16:04:46 -0500

    testing shared library

--- vars/masterFile.groovy
+++ vars/masterFile.groovy
@@ -1,8 +1,10 @@
 #!/usr/bin/env groovy
 def call(Closure body) {
     node {
+        def repo_name
         stage("SCM checkout") {
             checkout scm
+            repo_name = getRepoName()
         }
         stage("Test") {
             echo "no tests to run"

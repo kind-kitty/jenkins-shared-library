009823a3fe13f31a67ccb61fa9e9692ac0e58eda
Kavya K <srikavyakola2285@gmail.com>
2020-11-17 13:41:12 -0500

    testing shared libraries

--- vars/masterFile.groovy
+++ vars/masterFile.groovy
@@ -7,13 +7,14 @@ def call(Closure body) {
             checkout scm
             repo_name = getRepoName()
             commit_id = getCommitId()
+            echo constants.ECR_REGISTRY
         }
         stage("Test") {
             echo "no tests to run"
             
         }
         stage("docker build") {
-            docker.build("echo constants.ECR_REGISTRY/${repo_name}:${commit_id}", '.')
+            docker.build("constants.ECR_REGISTRY/${repo_name}:${commit_id}", '.')
             echo "docker build is successful"
         }
         stage("docker push") {

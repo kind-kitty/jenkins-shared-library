9d29b8dcad365bf1f354aae9ad531875cb959a58
Kavya K <srikavyakola2285@gmail.com>
2020-11-17 14:11:59 -0500

    testing shared libraries

--- vars/build.groovy
+++ vars/build.groovy
@@ -6,15 +6,4 @@ def call(){
     ecr_registry = constants.ECR_REGISTRY
     docker.build("${ecr_registry}/${repo_name}:${commit_id}", '.')
     echo "docker build is successful"
-    // return commit_id
-}
-
-
-// docker.build("${ecr_registry}/${repo_name}:${commit_id}", '.')
-//             echo "docker build is successful"
-
-
-// repo_name = getRepoName()
-// commit_id = getCommitId()
-// ecr_registry = constants.ECR_REGISTRY
         

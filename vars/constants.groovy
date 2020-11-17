c5e28fe58d4847b296f50ea2bc54514fab9d01d4
Kavya K <srikavyakola2285@gmail.com>
2020-11-17 14:07:01 -0500

    testing shared libraries

--- vars/constants.groovy
+++ vars/constants.groovy
@@ -1,4 +1,4 @@
 class Constants {
-  final String ECR_REGISTRY = "xxxx.us-east-1.amazonaws.com"
+  final String ECR_REGISTRY = "kavya.us-east-1.amazonaws.com"
   final String IAM_USER = "ecr:us-east-1:jenkins"
 }
